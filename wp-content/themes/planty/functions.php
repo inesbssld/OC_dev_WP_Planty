f5cfeb596e2d130c810b17664270399324077ed3
inesbssld <iboussel@yahoo.fr>
2023-06-17 12:45:03 +0200

    header fonctionnel

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -10,3 +10,27 @@ function theme_enqueue_styles(){
 
 
 }
+
+
+
+/**
+ * Add Menu Item to a specific place in the menu
+ */
+function ajouter_lien_admin($items, $args) {
+    // Vérifier si l'utilisateur est connecté et a le rôle d'administrateur
+    if (is_user_logged_in() && current_user_can('administrator')) {
+
+            if ($args->theme_location !== 'footer-menu') {
+
+        // Ajouter l'élément de menu
+        $element_menu = '<li class="lien_admin"><a href="#">Admin</a></li>';
+
+        // Insérer l'élément de menu en deuxième position
+        $items = substr_replace($items, $element_menu, strpos($items, '</li>', strpos($items, '<li>') + 1) + 5, 0);
+    }
+    }
+    return $items;
+}
+
+// Ajouter le hook pour exécuter la fonction d'ajout d'élément de menu
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin', 10, 2);
