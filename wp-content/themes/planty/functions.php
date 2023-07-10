944ecc84da92e058feda82f0130c58294780c019
inesbssld <iboussel@yahoo.fr>
2023-07-10 10:15:18 +0100

    version finale v2. dernières améliorations du responsif

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -20,10 +20,15 @@ function ajouter_lien_admin($items, $args) {
     // Vérifier si l'utilisateur est connecté et a le rôle d'administrateur
     if (is_user_logged_in() && current_user_can('administrator')) {
 
-            if ($args->theme_location !== 'kadence_footer') {
+// puis il exclut l'ajout de l'élément de menu si l'emplacement du menu est défini comme étant le menu du footer.
+            if ($args->theme_location !== 'footer') {
+
+        // Obtenir le lien d'administration du site WordPress
+
+            $admin_url = admin_url();
 
         // Ajouter l'élément de menu
-        $element_menu = '<li class="lien_admin"><a href="#">Admin</a></li>';
+           $element_menu = '<li class="lien_admin"><a href="' . $admin_url . '">Admin</a></li>';
 
         // Insérer l'élément de menu en deuxième position
         $items = substr_replace($items, $element_menu, strpos($items, '</li>', strpos($items, '<li>') + 1) + 5, 0);
